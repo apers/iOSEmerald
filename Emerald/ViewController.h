0e5c94f4e0d4b3bb7a47187a71746ae36299ff93
Audun Øygard <audunjoy@student.ifi.uio.no>
2016-01-14 10:07:33 +0100

    Initial commit

---
+++ Emerald/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emerald
+//
+//  Created by Audun Øygard on 13/01/16.
+//  Copyright (c) 2016 audunjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
